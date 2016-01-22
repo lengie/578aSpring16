b9fbc8bd91c1857f18160258587a6f917c6d15a9
lengie <cookiestork@gmail.com>
2016-01-22 00:23:45 -0800

    As sent to Ting Chen 11pm

--- HW1.cpp
+++ HW1.cpp
@@ -23,15 +23,16 @@ using namespace std;
 clock_t begin = clock();
 
 int wmatch =  3;
-int wmism  =  1;
+int wmism  =  -1;
 int windel = -3;
 
-vector<pair<int,int>> optPos;
+vector<pair<int,int> > optPos;
+string aligned1;
+string aligned2;
 
 int alignment_score(int **score,string seq1,string seq2){
 
-    OptPos.clear();
-
+    optPos.clear();
     for(int i=0;i<=seq1.size();++i)
     {
         for(int j=0;j<=seq2.size();++j){
@@ -50,12 +51,12 @@ int alignment_score(int **score,string seq1,string seq2){
                 score[i][j] = max(t1,max(t2,t3));
 
                 //keeping track of the position of optimal alignment
-                if(t1==max(t1,max(t2,t3))){
-                    OptPos.push_back(make_pair(i-1,j-1));
-                }else if(t2==max(t1,max(t2,t3))){
-                    OptPos.push_back(make_pair(i,"-"));
-                }else(t3==max(t1,max(t2,t3))){
-                    OptPos.push_back(make_pair("-",j));}
+                if(t1>score[i][j]){
+                    optPos.push_back(make_pair(i-1,j-1));
+                }else if(t2>score[i][j]){
+                    optPos.push_back(make_pair(i,'-'));
+                }else{
+                    optPos.push_back(make_pair('-',j));}
             }
         }
     }
@@ -71,20 +72,51 @@ int max_seq(string seq1,string seq2)
         score[i]=new int[seq2.size()+1];
     }
 
+    //I am working on this part -- considering how to make an array of previous positions
+    int **previous;
+    prev = new int*[seq1.size()+1];
+    for(int i=0;i<seq1.size()+1;++i)
+    {
+        prev[i]=new int[seq2.size()+1];
+    }
+
     return alignment_score(score,seq1,seq2);
 
 }
 
 void trace_back(string seq1,string seq2)
 {
-    OptPos
+    aligned1 = "";
+    aligned2 = "";
 
+    /*STILL TROUBLESHOOTING THIS PART. Previously I was not tracing back the one
+    previous-position-line of interest (from S[n][m] only).*/
+
+    //pair<int,int> endpt = make_pair(seq1.size(),seq2.size())
+    cout << seq1.size()<< endl;
+    cout << optPos[seq1.size()+1].first << " " << optPos[seq2.size()+1].second << endl;
+    /*while()
+        {
+        pair<int,int> step
+            if (optPos[i].first == '-'){
+                aligned1.append("-");
+                aligned2.push_back(seq2[i]);
+            }else if(optPos[i].second == '-'){
+                aligned1.push_back(seq1[i]);
+                aligned2.append("-");
+            }else{
+                aligned1.push_back(seq1[i]);
+                aligned2.push_back(seq2[i]);
+            }
+        }
+    cout << aligned1.size() << endl;
+    cout << aligned2.size() << endl;*/
 }
 
 //argv[1]:string file, argv[2] int wmatch, argv[3] int wmism, argv[4] int windel
 int main(int argc, char **argv)
 {
-    vector vector<string> species;
+    vector <vector<string> > species;
 
     struct paired{
         string species1;
@@ -123,7 +155,7 @@ int main(int argc, char **argv)
             }
             ++no;
             specname.push_back(read_lines);
-            cout << specname[no] << endl;
+            //cout << specname[no] << endl;
             getline(openfile,read_lines); //just to get the rest of the line out of the way
         }
         else if(read_lines.size() > 0)
@@ -139,7 +171,8 @@ int main(int argc, char **argv)
     /* Testing to see if it worked. Must use a loop can't cout whole thing
     for(int i=0; i<specname.size();i++){
         cout << specseq[i]<<endl;}*/
-
+    ofstream resfile;
+    resfile.open("HW1-output-LEngie.txt");
 
     for(int i=0;i<specname.size();++i){
             for(int j=0;j<specname.size();++j)
@@ -151,21 +184,21 @@ int main(int argc, char **argv)
                 align.species2 = specname[j];
                 cout << specname[i] << " " << specname[j] << endl;
                 align.alignmentscore = max_seq(specseq[i],specseq[j]);
-                cout << align.alignmentscore << endl;
-                //trace_back(specseq[i],specseq[j]);
+                //cout << align.alignmentscore << endl;
+                trace_back(specseq[i],specseq[j]);
+                resfile << "first sequence" << endl;
+                resfile << aligned1 << endl;
+                resfile << "second sequence" << endl;
+                resfile << aligned2 << endl;
                 }
             }
         }
 
-
-    //Traceback from value to actual sequence
-    /*T <<
-    ofstream myfile;
-    myfile.open("HW1-output-LEngie.txt");
-    myfile << T;
-    myfile.close();*/
+    resfile.close();
 
     return 0;
 }
 
+
+//Also I haven't corrected the CPU-time-calculating code
 clock_t end = clock();
