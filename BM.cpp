9089f78e9fc9c347b706265519889abbe5895c5d
lengie <cookiestork@gmail.com>
2016-03-06 12:34:48 -0800

    Extended bad char rule implemented

--- BM.cpp
+++ BM.cpp
@@ -21,10 +21,42 @@ clock_t begintime = clock();
 using namespace std;
 
 static void create_Rlist(const string &P, struct &Rlist){
-    Rlist.a; //wait need to revisit this
-    Rlist.c;
-    Rlist.g;
-    Rlist.t;
+    for(size_t i=P.size()-1; i=0; --i){
+        if(s[i]=='A'){
+            Rlist.a.push_back(i);
+        }
+        else if(s[i]=='C'){
+            Rlist.c.push_back(i);
+        }
+        else if(s[i]=='G'){
+            Rlist.g.push_back(i);
+        }
+        else if(s[i]=='T'){
+            Rlist.t.push_back(i);
+        }
+    }
+}
+static int lookupR(struct &Rlist, size_t &pos, char bp){
+ //Is it possible to do a binary search on a struct?
+    size_t i = 0;
+    if(bp=='A'){
+        while(Rlist.a[i]<=pos){
+            ++i;
+        }
+    } else if(bp=='C'){
+        while(Rlist.c[i]<=pos){
+                ++i;
+            }
+    } else if(bp=='G'){
+        while(Rlist.g[i]<=pos){
+            ++i;
+        }
+    }else{
+        while(Rlist.a[i]<=pos){
+            ++i;
+        }
+    }
+    return i;
 }
 
 static size_t
@@ -34,25 +66,25 @@ match(const string &s, size_t q, const size_t n) {
   return q;
 }
 
-static void z_box(const string &Z){
+static void good_suffix(const string &Lprime){
   size_t l = 0, r = 0;
   for (size_t k = 1; k < s.length(); ++k) {
     if (k >= r) { // Case 1: full comparison
-      Z[k] = match(s, 0, k);
-      if (Z[k] > 0) {
-        r = k + Z[k];
+      Lprime[k] = match(s, 0, k);
+      if (Lprime[k] > 0) {
+        r = k + Lprime[k];
         l = k;
       }
     }
     else { // Case 2: (we are inside a Z-box)
       const size_t k_prime = k - l;
       const size_t beta_len = r - k;
-      if (Z[k_prime] < beta_len) { // Case 2a: stay inside Z-box
-        Z[k] = Z[k_prime];
+      if (Lprime[k_prime] < beta_len) { // Case 2a: stay inside Z-box
+        Lprime[k] = Lprime[k_prime];
       }
       else {  // Case 2b: need to match outside the Z-box
         const size_t q = match(s, r, beta_len);
-        Z[k] = q - k;
+        Lprime[k] = q - k;
         r = q;
         l = k;
       }
@@ -84,26 +116,33 @@ int main(int argc, const char * const argv[]) {
     const size_t m = T.length();
     assert(n <= m);
 
-    struct Rs {
-        vector int a;
-        vector int c;
-        vector int g;
-        vector int t;
+    struct Rs{
+        vector<int> a;
+        vector<int> c;
+        vector<int> g;
+        vector<int> t;
     } Rlist;
 
     create_Rlist(P,Rlist)
 
-    vector<size_t> Z(n); //from the pattern?
-    z_box(Z);
+    vector<size_t> Lprime(n); //from the pattern?
+    good_suffix(Lprime);
 
-    for(int i=0;i<m-n;++i){
+    for(int i=0;i<m-n;){
+        size_t k=0;
         for(int j=n-1;j=0,--j){
             if(P[j]=T[j+i]){
                 k=k+1;
             }
+            else{ //mismatch
+                //Extended bad character rule
+                i=lookupR(&Rlist,j+i,T[j+i]);
+            }
         }
         if(k==n){
-        cout << "There's a matching occurence starting at " << j+1 << endl;
+            cout << "There's a matching occurrence starting at " << j+1 << endl;
+            i=i+n;
+        }
     }
 
     clock_t end = clock();
